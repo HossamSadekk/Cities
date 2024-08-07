5b187238b529972f1dd5f34d57b5e7085951224c
Hossam. <66652255+HossamSadekk@users.noreply.github.com>
2024-08-07 12:29:30 +0300

    removing unneeded dependency

--- app/src/main/java/com/example/cities/presentation/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/cities/presentation/homeScreen/HomeScreen.kt
@@ -3,16 +3,12 @@ package com.example.cities.presentation.homeScreen
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -21,12 +17,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
-import com.example.cities.R.font
 import com.example.cities.core.designSystem.components.LoaderWithOffset
 import com.example.cities.core.designSystem.components.MainAppBar
 import com.example.cities.core.designSystem.components.state.SearchWidgetState
@@ -37,10 +27,8 @@ import org.koin.androidx.compose.koinViewModel
 @Composable
 fun HomeScreen(viewModel: HomeScreenViewModel = koinViewModel()) {
     var searchWidgetState by remember { mutableStateOf(SearchWidgetState.CLOSED) }
-    var searchTextState by remember { mutableStateOf("") }
     val cities by viewModel.cities.collectAsState()
     val filterText by viewModel.filterText.collectAsState()
-    val loadingState by viewModel.isLoading.collectAsState()
     var isLoading by remember { mutableStateOf(true) }
 
     val context = LocalContext.current
